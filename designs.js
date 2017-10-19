6c9b004bcd3b6e492852a2a546015a4b58bda116
longvision <r.n.hori@gmail.com>
2017-10-19 16:08:39 -0200

    jquery doc added

--- designs.js
+++ designs.js
@@ -3,8 +3,27 @@
 
 // When size is submitted by the user, call makeGrid()
 
+
+const rows = $('#input_width');
+const columns = $('#input_height');
+
+
+
+
 function makeGrid() {
-
-// Your code goes here!
-
+  let wid = rows.val();
+  let hei = columns.val();
+  $('#pixel_canvas').empty();
+      for(let y=0;y < wid;y++){
+        $('#pixel_canvas').append('<tr></tr>');
+        for(let i=0;i<hei;i++){
+        $('<tr>').last().append('<td></td>');
+        }
+    }
 }
+
+$('#sizePicker').submit(function(event){
+  event.preventDefault();
+  makeGrid();
+
+});
